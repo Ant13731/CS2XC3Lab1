9a528c890e087b827b8063c26806f277ffb525a6
RitvikDeshpande <ritvikdeshpande@Ritviks-MacBook-Pro.local>
2022-01-18 15:48:15 -0500

    valid groups - Ritvik

--- code.py
+++ code.py
@@ -1,1 +1,14 @@
+def are_valid_groups(student_numbers, groups ):
+      
+  for group in groups:
+    for student_number in group:
+        for student in student_numbers:
+          if (student == student_number):
+              student_numbers.remove(student)
 
+  if student_numbers:
+    return False
+  else:
+    return True
+
+print(are_valid_groups([1234, 3920, 3402, 2390], [[0000, 9303, 3920 ], [7383, 2303, 3402, 1234 ], [2390]]))
