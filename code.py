026836355976e39c0d9581849da50ec1dcb73084
Parsa12565 <82552152+Parsa12565@users.noreply.github.com>
2022-01-18 16:34:28 -0500

    specifications

--- code.py
+++ code.py
@@ -2,17 +2,19 @@ def are_valid_groups(studentNums, groups):
     #All students are by default not in a group
     isStudentInGroup = []
     for x in range(len(studentNums)):
-        isStudentInGroup.append([False])
-        print("false\n")
+        isStudentInGroup.append(0)
     #If students are in a group, set their respective index to true
         for group in groups:
-            if studentNums[x] in group:
-                isStudentInGroup[x] = True
-                break
+            if len(group)<2 or len(group)>3:
+                return False
+            if  group.count(studentNums[x])==1:
+                isStudentInGroup[x]=isStudentInGroup[x]+1
+            elif group.count(studentNums[x])>1:
+                return False
     #If every student is in a group, return true, else return false
     for student in isStudentInGroup:
-        if student is False:
+        if student == 0 or student > 1:
             return False
     return True
-
-print(are_valid_groups(["1234", "3920", "3402", "2390"], [["0000", "9303", "3920" ], ["7383", "2303", "3402", "1234" ], ["2390"]]))
+#comment
+print(are_valid_groups(["1234", "3920", "3402", "2390"], [["9303", "3920", "1234" ], ["2303", "3402", "3402" ], ["2390", "0000"]]))
