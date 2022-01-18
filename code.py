c651bdb1fd74523974976645b2b770d2524ee34e
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2022-01-18 15:39:01 -0500

    add are_valid_groups to my code.py

--- code.py
+++ code.py
@@ -1,1 +1,15 @@
-
+def are_valid_groups(studentNums, groups):
+    #All students are by default not in a group
+    isStudentInGroup = []
+    for x in range(len(studentNums)):
+        isStudentInGroup[x] = False
+    #If students are in a group, set their respective index to true
+        for group in groups:
+            if studentNums[x] in group:
+                isStudentInGroup = True
+                break
+    #If every student is in a group, return true, else return false
+    for student in isStudentInGroup:
+        if student is False:
+            return False
+    return True
